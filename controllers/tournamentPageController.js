c92ad4b1845cc39523379ebcdf5c0ff49e6d7f1b
florianmoss <me@florianmoss.eu>
2020-03-28 21:08:28 +0000

    tournament page

--- controllers/tournamentPageController.js
+++ controllers/tournamentPageController.js
@@ -1,3 +1,14 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('database/players.db');
+
 module.exports = (req, res, next) => {
-    res.render('tournament');
+    db.serialize(() => {
+        db.all('SELECT * FROM results', (err, rows) => {
+            console.log(rows);
+
+            res.render('tournament', {
+                data: rows
+            });
+        });
+    });
 }
---
+++ database/create_results.js
@@ -0,0 +1,24 @@
+  const sqlite3 = require('sqlite3');
+  const db = new sqlite3.Database('players.db');
+
+  db.serialize(() => {
+
+      db.run("CREATE TABLE results (id INTEGER PRIMARY KEY AUTOINCREMENT, home TEXT, away TEXT, homeResult INTEGER, awayResult INTEGER, homeImage TEXT, awayImage TEXT)");
+
+      db.run("INSERT INTO results (home, away, homeResult, awayResult, homeImage, awayImage) VALUES ('LYIT', 'Cork', '3', '2', '/img/lyit.png', '/img/cork.png')");
+      db.run("INSERT INTO results (home, away, homeResult, awayResult, homeImage, awayImage) VALUES ('LYIT', 'NUIG', '2', '5', '/img/lyit.png', '/img/nuig.png')");
+      db.run("INSERT INTO results (home, away, homeResult, awayResult, homeImage, awayImage) VALUES ('LYIT', 'Sligo', '5', '2', '/img/lyit.png', '/img/sligo.png')");
+
+      db.run("INSERT INTO results (home, away, homeResult, awayResult, homeImage, awayImage) VALUES ('Cork', 'NUIG', '4', '1', '/img/cork.png', '/img/nuig.png')");
+      db.run("INSERT INTO results (home, away, homeResult, awayResult, homeImage, awayImage) VALUES ('Sligo', 'Cork', '3', '2', '/img/sligo.png', '/img/cork.png')");
+
+      db.run("INSERT INTO results (home, away, homeResult, awayResult, homeImage, awayImage) VALUES ('NUIG', 'Sligo', '0', '2', '/img/nuig.png', '/img/sligo.png')");
+
+
+      console.log('successfully created the positions results in players.db');
+
+      db.each("SELECT * FROM results", (err, row) => {
+          console.log(row.id + ": " + row.home + "  " + row.away);
+      });
+
+  })
